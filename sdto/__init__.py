3cd3cb84345b80fd4ad27b57ad9767202920fbcd
godpleaseno <zfrty@protonmail.com>
2022-10-12 22:18:29 +0300

    version bump

--- sdto/__init__.py
+++ sdto/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from sdto.sdto import (
     default_fingerprints,
@@ -8,4 +8,5 @@ from sdto.sdto import (
     scan_one,
     check_target,
     url_of,
+    parsed_fingerprints,
 )
